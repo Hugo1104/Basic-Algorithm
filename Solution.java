44d53ee60bc15933899c5f10421a8af0f2d76c6e
Hugo1104 <43482934+Hugo1104@users.noreply.github.com>
2021-01-30 21:48:30 +0800

    Add files via upload
    Minimum heap implementation

--- Solution.java
+++ Solution.java
@@ -1,164 +1,147 @@
 import java.io.*;
 import java.util.*;
-// Data structure for a node in a linked list
-class Item {
-    int data;
-    Item next;
 
-    Item(int data, Item next) {
-        this.data = data;
-        this.next = next;
-    }
+//person class
+class Person{
+    String name;
+    long score;
+    int pos;
 }
 
-// Data structure for representing a graph
-class Graph {
-    int n;  // # of nodes in the graph
+class MinHeap{
+    Person[] heap;
+    int size;
+    int maxsize;
 
-    Item[] A;
-    // For u in [0..n), A[u] is the adjecency list for u
-
-    Graph(int n) {
-        // initialize a graph with n vertices and no edges
-        this.n = n;
-        A = new Item[n];
+    public MinHeap(int maxsize){
+        this.maxsize = maxsize;
+        this.size = 0;
+        heap = new Person[maxsize];
     }
 
-    void addEdge(int u, int v) {
-        // add an edge u -> v to the graph
+    int parent(int pos){
+        if (pos <= 0) return -1;
+        return (pos - 1) / 2;
+    }
 
-        A[u] = new Item(v, A[u]);
+    private int leftChild(int pos){
+        if (pos >= size / 2) return -1;
+        return 2 * pos + 1;
+    }
+
+    private int rightChild(int pos){
+        if (pos >= (size - 1) / 2) return -1;
+        return 2 * pos + 2;
+    }
+
+    private boolean isLeaf (int pos){
+        if (pos >= (size / 2) && pos < size) {
+            return true;
+        }
+        return false;
+    }
+
+    void swap(int firstPos, int secPos){
+        Person temp;
+        temp = heap[firstPos];
+        heap[firstPos] = heap[secPos];
+        heap[firstPos].pos = firstPos;
+        heap[secPos] = temp;
+        heap[secPos].pos = secPos;
+    }
+
+    void minHeapify(int pos){
+        while (!isLeaf(pos)) {
+            int leftPos = leftChild(pos);
+            if ((leftPos < (size - 1)) && (heap[leftPos].score > heap[leftPos + 1].score)){
+                leftPos++; //leftPos is now the smaller element
+            }
+            if (heap[pos].score <= heap[leftPos].score) return;
+            swap(pos, leftPos);
+            pos = leftPos;
+        }
+    }
+
+    void insert(Person element){
+        if (size >= maxsize) {
+            return;
+        }
+
+        int current = size++;
+        heap[current] = element;
+
+        while (current!= 0 && (heap[current].score < heap[parent(current)].score)) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    void removeMin(){
+        if (size == 0) return;
+        swap(0, size - 1);
+        heap[size - 1] = null;
+        size--;
+        minHeapify(0);
     }
 }
-
-// Data structure holding data computed by DFS
-class DFSInfo {
-
-    // node colors
-    static final int WHITE = 0;
-    static final int GRAY  = 1;
-    static final int BLACK = 2;
-
-    int[] color;  // variable storing the color
-    // of each node during DFS
-    // (WHITE, GRAY, or BLACK)
-
-    int[] parent; // variable storing the parent
-    // of each node in the DFS forest
-
-    int d[];      // variable storing the discovery time
-    // of each node in the DFS forest
-
-    int f[];      // variable storing the finish time
-    // of each node in the DFS forest
-
-
-    DFSInfo(Graph graph) {
-        int n = graph.n;
-        color = new int[n];
-        parent = new int[n];
-        d = new int[n];
-        f = new int[n];
-    }
-}
-
-
-// your "main program" should look something like this:
 
 public class Solution {
 
-    static int time = 0;
-
-    static void recDFS(int u, Graph graph, DFSInfo info) {
-        // perform a recursive DFS, starting at u
-        info.color[u] = info.GRAY;
-        info.d[u] = ++time;
-        Item node = graph.A[u];
-        while (node != null){
-            if (info.color[node.data] == info.WHITE){
-                info.parent[node.data] = u;
-                recDFS(node.data, graph, info);
-            }
-            node = node.next;
-        }
-        info.color[u] = info.BLACK;
-        info.f[u] = ++time;
-    }
-
-    static DFSInfo DFS(Graph graph) {
-        // performs a "full" DFS on given graph
-        DFSInfo info = new DFSInfo(graph);
-        for (int j = 0; j < graph.n; j++) {
-            info.color[j] = info.WHITE;
-            info.parent[j] = j;
-        }
-        for (int i = 0; i < graph.n; i++) {
-            if (info.color[i] == info.WHITE){
-                recDFS(i, graph, info);
-            }
-        }
-        return info;
-    }
-
-    static Item findCycle(Graph graph, DFSInfo info) {
-        // If graph contains a cycle x_1 -> ... x_k -> x_1,
-        // return a pointer to the head of the linked list
-        // (x_1,..., x_k); otherwise, return null.
-        // NOTE: if there is a cycle, you should just return
-        // one cycle --- it does not matter which one.
-
-        // To do this, scan through the edges of graph,
-        // using info.f to locate a back edge.
-        // Once you find a back edge, use info.parent
-        // to build the list of nodes in the cycle
-        // in the correct order.
-        for (int i = 0; i < graph.n; i++) {
-            Item u = graph.A[i];
-            while (u != null){
-                if (info.f[i] < info.f[u.data]){
-                    Item cycleLast = new Item(i, u);
-                    return cycleLast;
-                }
-                u = u.next;
-            }
-        }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        //take in input
+    public static void main(String[] args){
+        //read file input and insert into the heap
         Scanner scanner = new Scanner(System.in);
+        int heap_size = Integer.parseInt(scanner.next());
 
-        //create the graph
-        int nodeSize = Integer.parseInt(scanner.next());
-        int edgeSize = Integer.parseInt(scanner.next());
-        Graph graph = new Graph(nodeSize);
-        for (int i = 0; i < edgeSize; i++) {
-            int u = Integer.parseInt(scanner.next());
-            int v = Integer.parseInt(scanner.next());
-            graph.addEdge(u - 1,v - 1);
+        //create a minHeap and a hashtable
+        MinHeap minHeap = new MinHeap(heap_size);
+        Hashtable hashtable = new Hashtable(heap_size);
+        for (int i = 0; i < heap_size; i++) {
+            Person newPerson = new Person();
+            newPerson.name = scanner.next();
+            newPerson.score = Long.parseLong(scanner.next());
+            newPerson.pos = i;
+
+            //store the element into minHeap and hashtable
+            minHeap.insert(newPerson);
+            hashtable.put(newPerson.name, newPerson);
         }
 
-        //perform DFS
-        DFSInfo info = DFS(graph);
-        Item cycleLast = findCycle(graph, info);
-        if (cycleLast != null){
-            ArrayList<Integer> cycle = new ArrayList<Integer>();
-            cycle.add(cycleLast.data);
-            int parent = info.parent[cycleLast.data];
-            while (parent != cycleLast.next.data){
-                cycle.add(parent);
-                parent = info.parent[parent];
-            }
-            cycle.add(parent);
-            System.out.println(1);
-            for (int i = cycle.size() - 1; i >= 0; i--) {
-                System.out.print(cycle.get(i) + 1 + " ");
-            }
+        int commandNum = Integer.parseInt(scanner.next());
+        for (int j = 0; j < commandNum; j++) {
+            int commandType = Integer.parseInt(scanner.next());
+            if (commandType == 1){
+                String updateName = scanner.next();
+                long updateScore = Long.parseLong(scanner.next());
+                Person updatePerson = (Person) hashtable.get(updateName);
+                int personPos = updatePerson.pos;
+                //update the score of person
+                updatePerson.score += updateScore;
 
-        }else{
-            System.out.println(0);
+                //update pos in the heap
+                minHeap.minHeapify(personPos);
+            }else{
+                long criteria = Long.parseLong(scanner.next());
+                while (minHeap.heap[0].score < criteria){
+                    minHeap.removeMin();
+                }
+                System.out.println(minHeap.size);
+
+            }
         }
+
+/*        Enumeration names = hashtable.keys();
+
+        while(names.hasMoreElements()){
+            String str = (String) names.nextElement();
+            Person stored = (Person) hashtable.get(str);
+            System.out.println(str + ":" + stored.score + " " + stored.pos);
+        }
+
+        System.out.println();
+
+        for (int h = 0; h < minHeap.size; h++) {
+            System.out.println(minHeap.heap[h].name + ": " + minHeap.heap[h].score + " " + minHeap.heap[h].pos);
+        }*/
     }
 
 }
